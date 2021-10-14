d0c92300a4464bdb617f7eecaee8c5e7e32db635
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-10-14 12:26:42 -0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import React from 'react';
 import './App.css';
 import Title from './components/Title';
 import HouseButtons from './components/HouseButtons';
 import StudentsList from './components/StudentsList';
 import data from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <Title />
-      <HouseButtons />
-      <StudentsList students={ data } />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Title />
+        <HouseButtons />
+        <StudentsList students={ data } />
+      </div>
+    );
+  }
 }
 
 export default App;
